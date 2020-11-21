09991a1ccce920cbd2f571c9711ef855037763f5
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2020-11-21 16:25:06 +0100

    Disable capture terminal in build script

--- build-install.sh
+++ build-install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build -t builder-dwm .
-docker run --rm -ti -v "$HOME/.local:/host/local" -v "$(pwd):/host/src" -w /host/src -u "$UID:$GROUPS" builder-dwm
+docker run --rm -v "$HOME/.local:/host/local" -v "$(pwd):/host/src" -w /host/src -u "$UID:$GROUPS" builder-dwm
